554bcd561b3ee7f24e51393baad09cf196cce359
madman-bob <madman.bob@hotmail.co.uk>
2018-03-02 10:54:11 +0000

    Add basic objects example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -31,6 +31,7 @@ class TestExamples(TestCase):
         "basic/function_argument.ou": "Hello, world\n",
         "basic/function_arguments.ou": "3\n",
         "basic/function_return_value.ou": "3\n",
+        "basic/objects.ou": "".join(str(line) + "\n" for line in [[0, 0], [1, 10], [1, 10, 100], [2, 0, 25]]),
         "intermediate/closure.ou": "".join(str(n) + "\n" for n in [1, 2, 3, 1, 2, 4, 3]),
         "intermediate/currying.ou": "3\n4\n5\n",
         "intermediate/first_class_functions.ou": "Hello\nHello\n",
