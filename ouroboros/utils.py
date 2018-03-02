be9186dcc7f442cd245b9640b5076f07e20eddc7
madman-bob <madman.bob@hotmail.co.uk>
2018-03-02 10:47:36 +0000

    Modify cached_class_property to play better with inheritance

--- ouroboros/utils.py
+++ ouroboros/utils.py
@@ -1,8 +1,9 @@
+from toolz import memoize
+
+
 class cached_class_property(object):
     def __init__(self, func):
-        self.func = func
+        self.func = memoize(func)
 
     def __get__(self, obj, cls):
-        value = self.func(cls)
-        setattr(cls, self.func.__name__, value)
-        return value
+        return self.func(cls)
