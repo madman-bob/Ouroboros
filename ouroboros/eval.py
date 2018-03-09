373c2bd5e76d728ddbc4c9e9e4f120e896f04444
madman-bob <madman.bob@hotmail.co.uk>
2018-03-09 14:58:57 +0000

    Update ouroboros_exec to return the executed block as an object, if no explicit return value is given

--- ouroboros/eval.py
+++ ouroboros/eval.py
@@ -3,6 +3,7 @@ from os import path
 from ouroboros.scope import Scope
 from ouroboros.sentences import Identifier, eval_sentence
 from ouroboros.contexts import StatementContext, BlockContext
+from ouroboros.internal_types import ObjectType
 from ouroboros.default_scope import default_scope
 
 __all__ = ('ouroboros_eval', 'ouroboros_exec', 'ouroboros_import')
@@ -21,7 +22,12 @@ def ouroboros_eval(expression_string, **variables):
 
 
 def ouroboros_exec(expression_string, **variables):
-    return ouroboros_interpret(BlockContext, expression_string, **variables)(())
+    return_value, scope = ouroboros_interpret(BlockContext, expression_string, **variables)((), return_scope=True)
+
+    if return_value is None:
+        return ObjectType(scope)
+
+    return return_value
 
 
 def ouroboros_import(file_handle, **variable):
