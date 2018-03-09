58bdb3bbc57e4d569cf014b71728dc5f5595241a
madman-bob <madman.bob@hotmail.co.uk>
2018-03-09 09:33:12 +0000

    Add option to include extra variables in scope when evaluating Ouroboros programs

--- ouroboros/eval.py
+++ ouroboros/eval.py
@@ -1,19 +1,22 @@
 from ouroboros.scope import Scope
-from ouroboros.sentences import eval_sentence
+from ouroboros.sentences import Identifier, eval_sentence
 from ouroboros.contexts import StatementContext, BlockContext
 from ouroboros.default_scope import default_scope
 
 __all__ = ('ouroboros_eval', 'ouroboros_exec')
 
 
-def ouroboros_interpret(interpretation_context, expression_string):
+def ouroboros_interpret(interpretation_context, expression_string, **variables):
     sentence, _ = interpretation_context.parse(expression_string)
-    return eval_sentence(sentence, Scope(parent_scope=default_scope))
+    return eval_sentence(sentence, Scope(
+        local_scope={Identifier(identifier): value for identifier, value in variables.items()},
+        parent_scope=default_scope
+    ))
 
 
-def ouroboros_eval(expression_string):
-    return ouroboros_interpret(StatementContext, expression_string)
+def ouroboros_eval(expression_string, **variables):
+    return ouroboros_interpret(StatementContext, expression_string, **variables)
 
 
-def ouroboros_exec(expression_string):
-    return ouroboros_interpret(BlockContext, expression_string)(())
+def ouroboros_exec(expression_string, **variables):
+    return ouroboros_interpret(BlockContext, expression_string, **variables)(())
