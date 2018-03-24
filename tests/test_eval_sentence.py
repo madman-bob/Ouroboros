9d6fdc395a39e5cc9828a9696b6717208b8391bd
madman-bob <madman.bob@hotmail.co.uk>
2018-03-24 15:53:38 +0000

    Add basic eval_sentence tests

---
+++ tests/test_eval_sentence.py
@@ -0,0 +1,71 @@
+from unittest import TestCase
+
+from toolz import curry
+
+from ouroboros.scope import Scope
+from ouroboros.lexer.lexical_tokens import Identifier, IntToken, Block
+from ouroboros.parser.parser import FunctionCall
+from ouroboros.eval_sentence import eval_sentence, eval_semantic_token
+
+
+class TestEvalToken(TestCase):
+    @staticmethod
+    @curry
+    def assign(identifier, value):
+        identifier, scope = identifier
+        scope[identifier] = eval_semantic_token(value)
+
+    def test_eval_constant(self):
+        self.assertEqual(
+            eval_sentence(IntToken(1), Scope({})),
+            1
+        )
+
+    def test_eval_identifier(self):
+        self.assertEqual(
+            eval_sentence(Identifier('x'), Scope({Identifier('x'): 1})),
+            1
+        )
+
+    def test_eval_function_call(self):
+        self.assertEqual(
+            eval_sentence(
+                FunctionCall(
+                    Identifier('f'),
+                    [IntToken(1), IntToken(2)]
+                ),
+                Scope({
+                    Identifier('f'): lambda x: lambda y: eval_semantic_token(x) + eval_semantic_token(y)
+                })
+            ),
+            3
+        )
+
+    def test_eval_block(self):
+        scope = Scope({
+            Identifier('x'): 1,
+            Identifier('+'): lambda x: lambda y: eval_semantic_token(x) + eval_semantic_token(y),
+            Identifier('='): self.assign
+        })
+
+        func = eval_sentence(
+            Block([
+                FunctionCall(
+                    Identifier('='),
+                    [
+                        Identifier('x'),
+                        FunctionCall(
+                            Identifier('+'),
+                            [Identifier('x'), IntToken(1)]
+                        )
+                    ]
+                )
+            ]),
+            scope
+        )
+
+        self.assertEqual(scope[Identifier('x')], 1)
+        func(())
+        self.assertEqual(scope[Identifier('x')], 2)
+        func(())
+        self.assertEqual(scope[Identifier('x')], 3)
